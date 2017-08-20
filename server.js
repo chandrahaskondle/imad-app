35547805b1a427db1769993442f28ce83765c7ef
chandrahaskondle <30701318+chandrahaskondle@users.noreply.github.com>
2017-08-20 23:06:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -86,9 +86,9 @@ app.get('/', function (req, res) {
 });
 
 var counter=0;
-app.get('/counter', fucntion (req, res) {
- counter = counter + 1;
- res.send(counter.toString());
+app.get('/counter', function(req, res) {
+    counter=counter + 1;
+    res.send(counter.toString());
 });
 
 app.get('/ui/style.css', function (req, res) {

414055324869caa85e1657e9b4ae9c6233cb033a
chandrahaskondle <30701318+chandrahaskondle@users.noreply.github.com>
2017-09-04 12:18:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -110,7 +110,7 @@ app.get('/test-db', function(req,res){
         if(err)
         res.status(500).send(err.toString());
         else
-        res.send(JSON.toString(result));
+        res.send(JSON.stringify(result));
     });
 });
 app.get('/ui/madi.png', function (req, res) {

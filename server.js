a4d24fbe41a2d851ed9a12f3495a75333932c7bd
chandrahaskondle <30701318+chandrahaskondle@users.noreply.github.com>
2017-09-08 23:14:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -119,12 +119,12 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 app.get('/articles/:articleName', function (req, res) {
-  pool.query("SELECT * FROM article WHERE article='"+req.params.articleName+"'",function(err,res){
+  pool.query("SELECT * FROM article WHERE article='"+req.params.articleName+"'",function(err,result){
     if(err){
         res.status(500).send(err.toString());
     } else{
          if(result.rows.length===0){
-            res.status(400).seend('Article not found');
+            res.status(404).seend('Article not found');
           }   
         else{
             var articleData=result.rows[0];
